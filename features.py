c87e6c0a2d467392184fcf1c36cc33af79ff8995
Sudeep Juvekar <sjuvekar@gmail.com>
2015-01-14 20:25:32 +0000

    more

--- features.py
+++ features.py
@@ -2,26 +2,30 @@ import cv2
 import cPickle
 import numpy
 import itertools
-import scipy.misc
+from scipy import misc
+import pandas
 
 SCALED_SIZE = (60, 60)
 
+def flatten_image(img):
+    return misc.imresize(img, SCALED_SIZE).ravel()
+ 
 def rotate(input_img, angle):
     rot = cv2.getRotationMatrix2D((input_img.shape[0]/2, input_img.shape[1]/2), angle, 1.0)
     #For some reason we have to swtich the output size dimensions here:
     rotated = cv2.warpAffine(input_img, rot, (input_img.shape[1], input_img.shape[0]), borderValue=255)
-    return misc.imresize(rotated, SCALED_SIZE)
+    return flatten_image(rotated)
 
 def rescale(input_img, scale):
     scaled = cv2.resize(input_img, dsize=None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-    return misc.imresize(scaled, SCALED_SIZE)
+    return flatten_image(scaled)
 
 def translate(input_img, x, y):
-    M = np.float32([[1,0,x],[0,1,y]])
+    M = numpy.float32([[1,0,x],[0,1,y]])
     #For some reason we have to swtich the output size dimensions here:
     new_shape = (input_img.shape[1] + max(0, x), input_img.shape[0] + max(0, y))
     dst = cv2.warpAffine(input_img, M, new_shape, borderValue=255)
-    return misc.imresize(dst, SCALED_SIZE)
+    return flatten_image(dst)
 
 def transform_state(state_name, state_img):
     """
@@ -31,21 +35,23 @@ def transform_state(state_name, state_img):
     angles = range(1, 360)
     numpy.random.shuffle(angles)
     angles = angles[0:30]
-    rotations = [rotate(state_img, angle) for angle in angles]
-    rotations["Method"] = "Rotate"
+    rotations = numpy.array( [rotate(state_img, angle) for angle in angles] )
+    #rotations["Method"] = "Rotate"
     
     # 2) Scale
     scale_sizes = numpy.random.rand(30) * 4
-    scales = [rescale(state_img, s) for s in scale_sizes]
-    scales["Method"] = "Scale"
+    scales = numpy.array( [rescale(state_img, s) for s in scale_sizes] )
+    #scales["Method"] = "Scale"
     
     # 3) Translate
-    trans_distances = numpy.random.rand(30) * 200
-    translations_all = itertools.product(translations, translations)
-    translations = [translate(state_img, dx, dy) for dx, dy in translations_all]
-    translations["Method"] = "Translate"
+    trans_distances = range(200)
+    numpy.random.shuffle(trans_distances)
+    trans_distances = trans_distances[0:50]
+    translations_all = itertools.product(trans_distances, trans_distances)
+    translations = numpy.array( [translate(state_img, dx, dy) for dx, dy in translations_all] )
+    #translations["Method"] = "Translate"
 
-    all_transformations = pandas.concat([rotations, scales, translations])
+    all_transformations = pandas.DataFrame( numpy.vstack( (rotations, scales, translations) ) )
     all_transformations["State"] = state_name
     return all_transformations
 
